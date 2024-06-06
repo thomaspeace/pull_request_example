b7ce8ad04bbd36a0093b567323c285f5b85aaf04
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-06 12:22:46 +0100

    add line 7 on main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Added this on line 5");
+
+        System.out.println("Added on line 7, on main");
     }
 
 }
